a47d314323c23b23b3073c9a7691dfdeb503fdb1
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2026-02-12 08:24:46 +0900

    Update itemList.js

--- itemList.js
+++ itemList.js
@@ -21,6 +21,9 @@ app.get("/itemList", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
+
 
 
