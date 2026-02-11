4441be09230fe8dc10423efa583a55ebfd7916c6
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2026-02-12 08:02:12 +0900

    Add files via upload

---
+++ itemList.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import { Pool } from "pg";
+
+const app = express();
+app.use(cors());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get("/itemList", async (req, res) => {
+  try {
+    const itemList = await pool.query("SELECT * FROM products");
+    res.json(itemList.rows);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(process.env.PORT);
