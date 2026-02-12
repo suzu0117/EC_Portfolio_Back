e45c2eca1680aa439a70600beb3c6acb1fc6390e
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2026-02-13 00:51:20 +0900

    Update itemList.js

--- itemList.js
+++ itemList.js
@@ -1,29 +1,30 @@
 import express from "express";
 import cors from "cors";
-import { Pool } from "pg";
+import {Pool} from "pg";
 
 const app = express();
 app.use(cors());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl:{
     rejectUnauthorized: false
   }
 });
 
-app.get("/itemList", async (req, res) => {
-  try {
+app.get("/itemList",async(req,res)=>{
+  try{
     const itemList = await pool.query("SELECT * FROM products");
     res.json(itemList.rows);
-  } catch (e) {
+  }catch (e) {
     console.log(e);
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT||3000,()=>{
+  console.log(`Server running on port ${process.env.PORT||3000}`);
 });
+
 
 
 
